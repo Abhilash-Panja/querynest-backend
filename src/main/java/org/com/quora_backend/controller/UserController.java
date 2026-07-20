355f2ba16f130253fe2eb27f9fcfc35cfa32a7b2
Abhilash Panja <abhilashpanja7@gmail.com>
2026-07-20 10:00:38 +0530

    Added the UserController

---
+++ src/main/java/org/com/quora_backend/controller/UserController.java
@@ -0,0 +1,42 @@
+package org.com.quora_backend.controller;
+
+import org.com.quora_backend.dto.user.CreateUserRequest;
+import org.com.quora_backend.dto.user.UpdateUserRequest;
+import org.com.quora_backend.dto.user.UserResponse;
+import org.com.quora_backend.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService){
+        this.userService=userService;
+    }
+    @PostMapping
+    public ResponseEntity<UserResponse> createUser(@RequestBody CreateUserRequest request){
+        UserResponse response  = userService.createUser(request);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(response);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponse> getUser(@PathVariable Long id){
+         UserResponse response = userService.getUserById(id);
+         return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<UserResponse> updateUser(@PathVariable Long id,
+                                                   @RequestBody UpdateUserRequest updateUserRequest){
+        UserResponse response = userService.updateUser(id,updateUserRequest);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id){
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
+}
