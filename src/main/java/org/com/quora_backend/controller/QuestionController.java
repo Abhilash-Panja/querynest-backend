1efa3baf09ed43af285cca71239af50498c6c9c4
Abhilash Panja <abhilashpanja7@gmail.com>
2026-07-26 04:46:02 +0530

    added user controller

---
+++ src/main/java/org/com/quora_backend/controller/QuestionController.java
@@ -0,0 +1,50 @@
+package org.com.quora_backend.controller;
+
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.com.quora_backend.dto.question.CreateQuestionRequest;
+import org.com.quora_backend.dto.question.QuestionResponse;
+import org.com.quora_backend.dto.question.QuestionSearchResponse;
+import org.com.quora_backend.dto.question.UpdateQuestionRequest;
+import org.com.quora_backend.service.QuestionService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/v1/questions")
+public class QuestionController {
+    private final QuestionService questionService;
+    @PostMapping
+    public ResponseEntity<QuestionResponse> createUser(@Valid @RequestBody CreateQuestionRequest request){
+        QuestionResponse response  = questionService.createQuestion(request);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(response);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<QuestionResponse> getUser(@PathVariable Long id){
+        QuestionResponse response = questionService.getQuestionById(id);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<QuestionResponse> updateQuestion(@PathVariable Long id,
+                                                       @Valid @RequestBody UpdateQuestionRequest updateQuestionRequest){
+        QuestionResponse response = questionService.updateQuestion(id,updateQuestionRequest);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletequestionById(@PathVariable Long id){
+        questionService.deletequestionById(id);
+        return ResponseEntity.noContent().build();
+    }
+    @GetMapping("/search")
+    public ResponseEntity<List<QuestionSearchResponse>> searchQuestions(@RequestParam String keyword){
+        List<QuestionSearchResponse> response = questionService.searchQuestions(keyword);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+}
