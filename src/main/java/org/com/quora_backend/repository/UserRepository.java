ce5a063d11dbbb1006a06b17df52ab0ef15e9c97
Abhilash Panja <abhilashpanja7@gmail.com>
2026-08-04 18:49:00 +0530

    updated the UserRepo by adding the finding user entity by userName

--- src/main/java/org/com/quora_backend/repository/UserRepository.java
+++ src/main/java/org/com/quora_backend/repository/UserRepository.java
@@ -5,8 +5,11 @@ import org.com.quora_backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
     boolean existsByUsername(String username);
     boolean existsByEmail(@Email(message = "Invalid email format") String email);
+    Optional<User> findByUsername(String username);
 }
